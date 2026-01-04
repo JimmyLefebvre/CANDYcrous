07642fb715a45c264d6399714580d966751b14a4
JimmyLefebvre <jimmy.lefebvre@etu.univ-amu.fr>
2026-01-04 19:14:02 +0100

    Fix difficulty menu and rules message wording
    Updated difficulty menu message and rules explanation.

--- main.cpp
+++ main.cpp
@@ -248,7 +248,7 @@ int main(){
 
     while (true) {
         clearScreen();
-        displayAMessage("\n|\tChoisissez votre difficultee\n|\t1 - FACILE\n|\t2 - NORMAL\n|\t3 - DIFFICILE\t\n|\t4 - pour un rappel des regles\t\n","Menu",39);
+        displayAMessage("\n|\tChoisissez votre difficultee\n|\t1 - FACILE\n|\t2 - NORMAL\n|\t3 - DIFFICILE\t\n|\t4 - Rappel des regles\t\n","Menu",39);
         Mode=getEntier();
         if(Mode == 4){
             displayAMessage("Les regles sont simples;\n\t-Tout d'abord vous devez choisir votre mode de jeux\n\t-Ensuite vous verrez une grille de chiffres de couleur\n\t-Ceux-ci disparaissent lorsque vous en alignez 3 ou plus\n\t-Lorsque vous les faites disparaitre, vous gagnez des points\n\t-Votre objectif est d'obtenir le score le plus haut possible, bonne partie!\n","CANDYcrous",40);
