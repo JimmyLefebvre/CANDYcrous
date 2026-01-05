038f9eab266607c0a7f5a0f517c67d6e4a463bb6
Chevremont Lilian <lilian.chevremont@etu.univ-amu.fr>
2026-01-05 01:19:34 +0100

    finalisation prêt à être rendu

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 #include <ctime>
 #include <chrono>
 #include <thread>
+#include <algorithm>
 using namespace std;
 
 void clearScreen () {
     cout << "\033[H\033[2J";
 }
-
+// définition des couleurs
 const unsigned KReset   (0);
 const unsigned KNoir    (40);
 const unsigned KRouge   (41);
@@ -18,27 +19,56 @@ const unsigned KJaune   (43);
 const unsigned KBleu    (44);
 const unsigned KMAgenta (45);
 const unsigned KCyan    (46);
+
+//création de la liste qui li couleur et numéros de cases
 vector<unsigned> couleurs ={KNoir,KRouge,KVert,KJaune,KBleu,KMAgenta,KCyan};
+
+//valeur maximale des cases
 const unsigned int KNbCandies = 6;
+
+//valeur des cases retirées
 const unsigned Kimpossible = 0;
+
+//variable demander pour le bon fonctionnement du jeux
 int coup = 0;
 int score = 0;
 bool infinite = false;
-
 int test = 1;
+
 typedef vector <unsigned> line; // un type représentant une ligne de la grille
 typedef vector <line> mat; // un type représentant la grille
 
 struct maposition {
     unsigned abs;
     unsigned ord;
-}; // une position dans la girlle
+};
+
+// variable contenant les cases qui ne peuvent pas être déplacées
+vector<maposition> UnmoovableBlock;
+
+inline bool operator==(const maposition& a, const maposition& b) noexcept {
+    return a.ord == b.ord && a.abs == b.abs;
+}
 
 
+//fonction initialisation du mode cases immobiles
+vector<maposition> InitUnmoovableBlock(mat & grid){
+    vector<maposition> block;
+    block.resize(grid.size()*grid.size()/9);
+    for(size_t i = 0; i < grid.size()*grid.size()/9;++i){
+        block[i].abs = rand() % grid.size();
+        block[i].ord = rand() % grid.size();
+    }
+    return block;
+}
+
+
+//fonction de changement de couleur
 void couleur (const unsigned & coul) {
     cout << "\033[" << coul <<"m";
 }
 
+// fonction pour récupérer seulement un entier
 int getEntier(){
     while(true){
         string str;
@@ -53,6 +83,7 @@ int getEntier(){
     }
 }
 
+// fonction pour récupérer seulement un caractère
 char getChar(){
     while(true){
         string str;
@@ -60,13 +91,14 @@ char getChar(){
         char charRenvoye;
         if(not str.empty() && not isdigit(str[0])){
             charRenvoye = str[0];
-            return charRenvoye;
+            return tolower(charRenvoye);
         }else{
             cout<<"la valeur n'est pas valide"<<endl;
         }
     }
 }
 
+// initialisation de la grille
 void initGrid(mat &grid, size_t matSize) {
     grid.resize(matSize);
     for (size_t x = 0; x < matSize; ++x) {
@@ -79,7 +111,8 @@ void initGrid(mat &grid, size_t matSize) {
     }
 }
 
-void  displayGrid (const mat & grid , maposition & pos){
+// fonction d'affichage de la grille
+void  displayGrid (const mat & grid , maposition & pos,const vector<maposition> & block){
     clearScreen ();
     couleur(KReset);
     cout<<"O/A";
@@ -100,13 +133,18 @@ void  displayGrid (const mat & grid , maposition & pos){
         for (size_t y = 0; y < grid[x].size(); ++y) {
             if(grid[x][y] <= 0 or grid[x][y] > KNbCandies){
                 if(pos.ord==x and pos.abs==y){
-                    cout<<" [/]";
+                    cout<<" >-<";
                 }else{
-                cout<<"  / ";
+                cout<<"  - ";
                 }
             }else{
                 cout<<" ";
-                couleur (couleurs[grid[x][y]]);
+                auto IndexBlockUnmoov = find(block.begin(),block.end(),maposition{static_cast<unsigned int>(x),static_cast<unsigned int>(y)});
+                if(IndexBlockUnmoov != block.end()){
+                    couleur (KReset);
+                }else{
+                    couleur (couleurs[grid[x][y]]);
+                }
                 if(pos.ord==x and pos.abs==y){
                    cout<<">"<<grid[x][y]<<"<";
                 }else{
@@ -120,28 +158,37 @@ void  displayGrid (const mat & grid , maposition & pos){
     cout<<endl;
 }
 
-void makeAMove (mat & grid, const maposition & pos, const char & direction){
-
-    if (pos.abs < grid.size() && pos.ord < grid[pos.abs].size()){
+// fonction pour faire un mouvement
+void makeAMove (mat & grid, const maposition & pos, const char & direction,const vector<maposition> & block){
+    auto IndexBlockUnmoov = find(block.begin(),block.end(),pos);
+    if (pos.abs < grid.size() && pos.ord < grid[pos.abs].size() && grid[pos.ord][pos.abs]!= 0 && IndexBlockUnmoov == block.end()){
         ++coup;
-        if(tolower(direction) == 's' && pos.ord < grid.size() - 1 && grid[pos.ord+1][pos.abs] != 0){
+        auto IndexBlockUnmoovDown = find(block.begin(),block.end(),maposition{pos.ord+1,pos.abs});
+        auto IndexBlockUnmoovUp = find(block.begin(),block.end(),maposition{pos.ord-1,pos.abs});
+        auto IndexBlockUnmoovLeft = find(block.begin(),block.end(),maposition{pos.ord,pos.abs-1});
+        auto IndexBlockUnmoovRight = find(block.begin(),block.end(),maposition{pos.ord,pos.abs+1});
+
+        if(direction == 's' && pos.ord < grid.size() - 1 && grid[pos.ord+1][pos.abs] != 0 && IndexBlockUnmoovDown == block.end()){
             swap(grid[pos.ord][pos.abs],grid[pos.ord + 1][pos.abs]);
-        }else if(tolower(direction) == 'z' && pos.ord > 0 && grid[pos.ord-1][pos.abs] != 0){
+        }else if(direction == 'z' && pos.ord > 0 && grid[pos.ord-1][pos.abs] != 0 && IndexBlockUnmoovUp == block.end()){
             swap(grid[pos.ord][pos.abs],grid[pos.ord - 1][pos.abs]);
-        }else if(tolower(direction) == 'a' && pos.abs > 0 && grid[pos.ord][pos.abs-1] != 0){
+        }else if(direction == 'a' && pos.abs > 0 && grid[pos.ord][pos.abs-1] != 0 && IndexBlockUnmoovLeft == block.end()){
             swap(grid[pos.ord][pos.abs],grid[pos.ord][pos.abs - 1]);
-        }else if(tolower(direction) == 'e' && pos.abs < grid.size()-1 && grid[pos.ord][pos.abs+1] != 0){
+        }else if(direction == 'e' && pos.abs < grid.size()-1 && grid[pos.ord][pos.abs+1] != 0 && IndexBlockUnmoovRight == block.end()){
             swap(grid[pos.ord][pos.abs],grid[pos.ord][pos.abs + 1]);
         }else{
-            cout<<"mauvais instruction"<<endl;
+            --coup;
+            cout<<"mauvaise instruction"<<endl;
             this_thread::sleep_for(chrono::seconds(2));
         }
     }else{
+        if (IndexBlockUnmoov != block.end())
         cout<<"mauvais indice"<<endl;
         this_thread::sleep_for(chrono::seconds(2));
     }
 }
 
+// fonctions pour savoir si au moins trois blocs identiques sont alignés verticalement et horizontalement
 bool atLeastThreeInAColumn(const mat &grid, maposition &pos, unsigned &howMany) {
     for (size_t x = 0; x < grid.size(); ++x) {
         for (size_t y = 0; y < grid.size(); ++y) {
@@ -182,6 +229,7 @@ bool atLeastThreeInARow(const mat &grid, maposition &pos, unsigned &howMany) {
     return false;
 }
 
+// fonctions pour retirer les blocs identiques alignés verticalement et horizontalement
 void removalInColumn (mat & grid, const maposition & pos, unsigned  howMany){
     size_t y;
     for(size_t i = 0; i < howMany; ++i){
@@ -202,6 +250,7 @@ void removalInRow (mat & grid, const maposition & pos, unsigned  howMany){
     }
 }
 
+// fonction d'affichage des menu et textes importants pour le jeux
 void displayAMessage(const string & texte,const string & title,size_t size){
     if(size == 0)size = texte.size();
     cout<<"#="<<title;
@@ -217,6 +266,7 @@ void displayAMessage(const string & texte,const string & title,size_t size){
     cout<<"#"<<endl;
 }
 
+// fonction nécessaire pour le mode infini, afin de recompléter la grille à chaque suppression
 void completeGrid(mat & grid){
     for (size_t x = 0; x < grid.size(); ++x) {
         for (size_t y = 0; y < grid.size(); ++y) {
@@ -227,7 +277,7 @@ void completeGrid(mat & grid){
     }
 }
 
-
+//fonction principale
 int main(){
     srand(time(0));
 
@@ -238,88 +288,115 @@ int main(){
     pos.abs=0;
     pos.ord=0;
 
-    unsigned Mode;
     int Times = 0;
     unsigned howMany;
     char moveInstr;
 
-    displayAMessage("\n|\tfait par\n|\tJimmy LEFEVBRE et Lilian CHEVREMONT G2A1\n|\n|\tbonne partie\t\tsae 1.01\n","CANDYcrous",47);
+    //menu 1
+    displayAMessage("\n|\tfait par\n|\tJimmy LEFEVBRE et Lilian CHEVREMONT G2\n|\n|\tbonne partie\t\tsae 1.01\n","CANDYcrous",50);
     this_thread::sleep_for(chrono::seconds(2));
 
+
+    unsigned Mode;
     while (true) {
-        clearScreen();
-        displayAMessage("\n|\tChoisissez votre difficultee\n|\t1 - FACILE\n|\t2 - NORMAL\n|\t3 - DIFFICILE\t\n|\t4 - Rappel des regles\t\n","Menu",39);
-        Mode=getEntier();
-        if(Mode == 4){
-            displayAMessage("Les regles sont simples;\n\t-Tout d'abord vous devez choisir votre mode de jeux\n\t-Ensuite vous verrez une grille de chiffres de couleur\n\t-Ceux-ci disparaissent lorsque vous en alignez 3 ou plus\n\t-Lorsque vous les faites disparaitre, vous gagnez des points\n\t-Votre objectif est d'obtenir le score le plus haut possible, bonne partie!\n","CANDYcrous",40);
-            this_thread::sleep_for(chrono::seconds(3));
-        }else if(Mode == 1){
-            taille = 6;
-            Times = (taille*taille)/2;
-            break;
-        }else if(Mode == 2){
-            taille = 12;
-            Times = (taille*taille)/4;
-            break;
-        }else if(Mode == 3){
-            taille = 24;
-            Times = (taille*taille)/6;
-            break;
-        }
-    }
-
-    while (true) {
-        clearScreen();
-        displayAMessage("\n|\tChoisissez votre mode de jeux\n|\t1 - NORMAL\n|\t2 - INFINI\n|\n","Menu",39);
-        Mode=getEntier();
-        if(Mode == 1){
-            break;
-        }else if(Mode == 2){
-            infinite = true;
-            break;
-        }
-    }
-
-    initGrid(grid,taille);
-    const int KmaxTimes = Times;
-
-    while(KmaxTimes > coup){
-        displayGrid(grid,pos);
-
-        displayAMessage("coup : "+to_string(coup)+"/"+to_string(KmaxTimes)+"   score:"+to_string(score),"",0);
-
-        displayAMessage("Veuillez selectionner une abscisse","",0);
-        pos.abs=getEntier();
-        --pos.abs;
-        displayGrid(grid,pos);
-
-        displayAMessage("coup : "+to_string(coup)+"/"+to_string(KmaxTimes)+"   score:"+to_string(score),"",0);
-        displayAMessage("Veuillez selectionner une ordonnee","",0);
-        pos.ord=getEntier();
-        --pos.ord;
-        displayGrid(grid,pos);
-
-        displayAMessage("coup : "+to_string(coup)+"/"+to_string(KmaxTimes)+"   score:"+to_string(score),"",0);
-        displayAMessage("Veuillez selectionner un mouvement via les touches a(gauche),e(droite),z(haut),s(bas),/(pour arreter)","",0);
-        moveInstr = getChar();
-        if(moveInstr == '/'){
-            break;
-        }
-
-        makeAMove(grid,pos,moveInstr);
-
-        while(atLeastThreeInARow(grid,pos,howMany) or atLeastThreeInAColumn(grid,pos,howMany)){
-            if(atLeastThreeInARow(grid,pos,howMany)){
-                removalInRow(grid,pos,howMany);
-            }
-            else if(atLeastThreeInAColumn(grid,pos,howMany)){
-                removalInColumn(grid,pos,howMany);
+        //menu 2
+        while (true) {
+            clearScreen();
+            displayAMessage("\n|\tChoisissez votre difficultee\n|\t1 - FACILE\n|\t2 - NORMAL\n|\t3 - DIFFICILE\t\n|\t4 - Rappel des regles\t\n","Menu",39);
+            Mode=getEntier();
+            if(Mode == 4){
+                displayAMessage("Les regles sont simples:\n|\t-Tout d'abord vous devez choisir votre mode de jeux\n|\t-Ensuite vous verrez une grille de chiffres de couleur\n|\t-Ceux-ci disparaissent lorsque vous en alignez 3 ou plus\n|\t-Lorsque vous les faites disparaitre, vous gagnez des points\n|\t-Votre objectif est d'obtenir le score le plus haut possible, bonne partie!\n","CANDYcrous",90);
+                this_thread::sleep_for(chrono::seconds(7));
+            }else if(Mode == 1){
+                taille = 6;
+                Times = (taille*taille)/2;
+                break;
+            }else if(Mode == 2){
+                taille = 12;
+                Times = (taille*taille)/4;
+                break;
+            }else if(Mode == 3){
+                taille = 24;
+                Times = (taille*taille)/6;
+                break;
             }
         }
-        if(infinite){
-            completeGrid(grid);
+        //initialisation de la grille
+        initGrid(grid,taille);
+        //maximum de coup
+        const int KmaxTimes = Times;
+
+        //menu 2
+        while (true) {
+            clearScreen();
+            displayAMessage("\n|\tChoisissez votre mode de jeux\n|\t1 - NORMAL\n|\t2 - INFINI\n|\t3 - CASES IMMOBILES \n|\n","Menu",39);
+            Mode=getEntier();
+            if(Mode == 1){
+                break;
+            }else if(Mode == 2){
+                infinite = true;
+                break;
+            }else if(Mode == 3){
+                UnmoovableBlock = InitUnmoovableBlock(grid);
+                break;
+            }
+        }
+
+        //boucle de la partie
+        while(KmaxTimes > coup){
+            displayGrid(grid,pos,UnmoovableBlock);
+
+            displayAMessage("coup : "+to_string(coup)+"/"+to_string(KmaxTimes)+"   score:"+to_string(score),"",0);
+
+            displayAMessage("Veuillez selectionner une abscisse (ou faites 0 pour quitter la partie)","",0);
+            pos.abs=getEntier();
+            if(pos.abs == 0){
+                break;
+            }
+            --pos.abs;
+            displayGrid(grid,pos,UnmoovableBlock);
+
+            displayAMessage("coup : "+to_string(coup)+"/"+to_string(KmaxTimes)+"   score:"+to_string(score),"",0);
+            displayAMessage("Veuillez selectionner une ordonnee (ou faites 0 pour quitter la partie)","",0);
+            pos.ord=getEntier();
+            if(pos.ord == 0){
+                break;
+            }
+            --pos.ord;
+            displayGrid(grid,pos,UnmoovableBlock);
+
+            displayAMessage("coup : "+to_string(coup)+"/"+to_string(KmaxTimes)+"   score:"+to_string(score),"",0);
+            displayAMessage("Veuillez selectionner un mouvement via les touches a(gauche),e(droite),z(haut),s(bas),/(pour quitter la partie)","",0);
+            moveInstr = getChar();
+            if(moveInstr == '/'){
+                break;
+            }
+
+            makeAMove(grid,pos,moveInstr,UnmoovableBlock);
+
+            while(atLeastThreeInARow(grid,pos,howMany) or atLeastThreeInAColumn(grid,pos,howMany)){
+                if(atLeastThreeInARow(grid,pos,howMany)){
+                    removalInRow(grid,pos,howMany);
+                }
+                else if(atLeastThreeInAColumn(grid,pos,howMany)){
+                    removalInColumn(grid,pos,howMany);
+                }
+            }
+            if(infinite){
+                completeGrid(grid);
+            }
+        }
+        displayAMessage("\n|\tVous avez fini votre partie avec "+to_string(coup)+" coups ce qui vous fait un score de "+to_string(score)+"\t|\n|\n|\t (n'hesitez pas a tester d'autres difficultees)\n","!FINI!",0);
+        displayAMessage("Voulez-vous rejouer? y/n","Menu",0);
+        while(true){
+            char rePlay;
+            rePlay = tolower(getChar());
+            if(rePlay == 'n'){
+                return 0;
+            }else if(rePlay == 'y'){
+                break;
+            }
+            cout<<"veuillez choisir entre y et n"<<endl;
         }
     }
-    displayAMessage("\n|\tVous avez fini votre partie avec "+to_string(coup)+" coups ce qui vous fait un score de "+to_string(score)+"\t|\n|\n|\t (n'hesitez pas a tester d'autres difficultees)\n","!FINI!",0);
-    return 0;
 }
